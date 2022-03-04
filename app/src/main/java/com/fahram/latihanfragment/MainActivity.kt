e232e8c9b992837635b72e2e60c0c25aede0141d
Khaidir Fahram <khaidir@fahram.com>
2022-03-04 08:47:46 +0700

    Fragment in Activity

--- app/src/main/java/com/fahram/latihanfragment/MainActivity.kt
+++ app/src/main/java/com/fahram/latihanfragment/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+        if (fragment !is HomeFragment) {
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
